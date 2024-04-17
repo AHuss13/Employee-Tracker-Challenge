5bfbb89f0b9b60bb67d2d09df2cf139778a091a7
Adam Huss <adamhuss13@gmail.com>
2024-04-16 19:59:44 -0500

    added server

---
+++ server.js
@@ -0,0 +1,20 @@
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    database: 'employee_db',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: ''
+  },
+
+  console.log(`Connected to the employee_db database.`)
+);
